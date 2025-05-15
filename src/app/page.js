bebc125b1b8e34b4214421b089f57efba56606a2
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-15 22:03:23 +0100

    What a big one

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost/warehouse/backend/api/index.php', {
+      const res = await fetch('https:/basefood.trendsaf.co/api/test', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
